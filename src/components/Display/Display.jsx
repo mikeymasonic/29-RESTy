1730c7dcadedce7d9d3bb0d7b2bc25f649562731
Michael Romay <amerror@gmail.com>
2020-05-01 11:55:34 -0700

    imported json view library

--- src/components/Display/Display.jsx
+++ src/components/Display/Display.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import ReactJson from 'react-json-view';
 import styles from './Display.css';
 
 const Display = ({ response }) => {
-  const responseList = Object.keys(response).map(keyValue => {
-    return <li key={keyValue}>{keyValue}: {response[keyValue]}</li>;
-  });
+  // const responseList = Object.keys(response).map(keyValue => {
+  //   return <li key={keyValue}>{keyValue}: {response[keyValue]}</li>;
+  // });
 
   return (
     <div className={styles.Display}>
-      <p>{responseList}</p>
+      <ReactJson src={response} theme="monokai" name="Response" />
     </div>
   );
 };
