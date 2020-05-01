b2becfa824c15ccf425c1f6d169f75a33706347e
Michael Romay <amerror@gmail.com>
2020-05-01 13:36:22 -0700

    added headers to display react json

--- src/components/Display/Display.jsx
+++ src/components/Display/Display.jsx
@@ -3,20 +3,25 @@ import PropTypes from 'prop-types';
 import ReactJson from 'react-json-view';
 import styles from './Display.css';
 
-const Display = ({ response }) => {
+const Display = ({ response, headers }) => {
   // const responseList = Object.keys(response).map(keyValue => {
   //   return <li key={keyValue}>{keyValue}: {response[keyValue]}</li>;
   // });
 
   return (
     <div className={styles.Display}>
+      <ReactJson src={headers} theme="monokai" name="Headers"/>
       <ReactJson src={response} theme="monokai" name="Response" />
     </div>
   );
 };
 
 Display.propTypes = {
-  response: PropTypes.string
+  headers: PropTypes.object,
+  response: PropTypes.oneOfType([
+    PropTypes.object,
+    PropTypes.array
+  ]),
 };
 
 export default Display;
