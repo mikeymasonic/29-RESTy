fc250e5f603966cda36d6f7ca74a419bb1fe0057
Michael Romay <amerror@gmail.com>
2020-05-01 16:04:14 -0700

    list

---
+++ src/components/List/List.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './List.css';
+
+const List = ({ requests, handleClear }) => {
+  const requestNodes = requests.map(request => {
+    const home = request.url.split('/')[2];
+    const user = request.url.split('.com')[1];
+    return <li key={request.url}>
+      <h3>{request.method}</h3>
+      <p>{home}</p>
+      <p>{user}</p>
+    </li>;
+  });
+
+  return (
+    <div className={styles.List}>
+      <ul>
+        {requestNodes}
+      </ul>
+      <button onClick={handleClear}>Clear</button>
+    </div>
+  );
+};
+
+List.propTypes = {
+  requests: PropTypes.array,
+  handleClear: PropTypes.func
+};
+
+export default List;
