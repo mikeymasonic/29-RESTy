fc112caabf0585f15967c59a0e74fe7735adeadb
Michael Romay <amerror@gmail.com>
2020-05-01 13:43:55 -0700

    added fetch headers to api

--- src/services/api.js
+++ src/services/api.js
@@ -2,3 +2,14 @@ export const fetchResponse = (url, request) => {
   return fetch(url, request)
     .then(res => res.json());
 };
+
+export const fetchHeaders = (url, request) => {
+  return fetch(url, request)
+    .then(res => {
+      let headers = {};
+      for(let header of res.headers.entries()) {
+        headers[header[0]] = header[1];
+      }
+      return headers;
+    });
+};
